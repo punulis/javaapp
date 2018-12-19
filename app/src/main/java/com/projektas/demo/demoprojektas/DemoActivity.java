5181e0b59c70fd5044e7dc39b1722bd242c12f99
punulis <punulis@gmail.com>
2018-12-19 13:34:06 +0200

    initial commit + login + search(demo)

---
+++ app/src/main/java/com/projektas/demo/demoprojektas/DemoActivity.java
@@ -0,0 +1,22 @@
+package com.projektas.demo.demoprojektas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+
+public class DemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        Toast.makeText(DemoActivity.this, "Laba diena", Toast.LENGTH_LONG().show();
+
+
+
+    }
+
+}
